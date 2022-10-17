1e641b36d82268496a104f0bc1a2cb2e30281d8d
libingle <2523890936@qq.com>
2022-10-17 11:45:58 +0800

    首页设计完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<HeaderIndex></HeaderIndex>
+<Home></Home>
+   <FooterIndex></FooterIndex>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import  Home from './views/Home'
+import FooterIndex from "@/components/Footer/FooterIndex";
+import HeaderIndex from "@/components/Header/HeaderIndex";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,Home,FooterIndex,HeaderIndex
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
